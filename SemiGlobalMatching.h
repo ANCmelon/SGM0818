15eb4e14906c8f629afe751ff7743e7d53d57647
ANCmelon <sunzanzan_186@163.com>
2023-08-19 15:47:05 +0800

    [init commit][0819]{image census  cost}

---
+++ SemiGlobalMatching.h
@@ -0,0 +1,28 @@
+
+
+
+
+
+class SemiGlobalMatching
+{
+private:
+    /* data */
+    int Width;
+    int Height;
+    int LeftImage[];
+    int RightImage[];
+    
+public:
+    SemiGlobalMatching(/* args */);
+    ~SemiGlobalMatching();
+};
+
+SemiGlobalMatching::SemiGlobalMatching(/* args */)
+{
+
+}
+
+SemiGlobalMatching::~SemiGlobalMatching()
+{
+
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    printf("0818");
+    // std::cout << "0818" << std::endl; 
+    return 0;
+}
